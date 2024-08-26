afdb020a9056ecc370e642bfe908b91418f0382d
Claire Froelich <claire.froelich@gmail.com>
2024-08-26 19:43:02 -0400

    increase rate limit

--- src/middleware/limiter.ts
+++ src/middleware/limiter.ts
@@ -2,6 +2,6 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 60 * 1000, // n seconds window
-  max: 8, // limit calls per IP per windowMs
+  max: 15, // limit calls per IP per windowMs
   message: "Too many requests from this IP, please try again later.",
 });
